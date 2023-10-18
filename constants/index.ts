8db0ed9432f64668f803d83987db3a66dd6d3000
OtaraAlex <mbechealex61@gmail.com>
2023-10-18 21:33:35 +0300

    Let's travel and camp

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Home" },
+  { href: "/", key: "how_explorez_works", label: "How Explorez Works?" },
+  { href: "/", key: "services", label: "Services" },
+  { href: "/", key: "pricing ", label: "Pricing " },
+  { href: "/", key: "contact_us", label: "Contact Us" },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "Offline maps",
+    icon: "/map.svg",
+    variant: "green",
+    description:
+      "We offer a solution that enables you to use our application for climbing, even in offline mode. You can access offline maps anytime, ensuring you have navigation assistance even in signal-free locations",
+  },
+  {
+    title: "Schedule your adventure",
+    icon: "/calendar.svg",
+    variant: "green",
+    description:
+      "Plan an adventure with friends and take advantage of Explorez's enticing holiday offers. With these exciting deals, there's no need for further deliberation",
+  },
+  {
+    title: "Augmented reality",
+    icon: "/tech.svg",
+    variant: "green",
+    description:
+      "We use the latest technology to make your climbing experience more enjoyable and safe. We use augmented reality to provide you with a more realistic experience",
+  },
+  {
+    title: "New locations every month",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "We are constantly updating our database to provide you with the most up-to-date information. We add new locations every month to ensure that you have a wide range of options to choose from",
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Learn More",
+    links: [
+      "About Explorez",
+      "Press Releases",
+      "Environment",
+      "Jobs",
+      "Privacy Policy",
+      "Contact Us",
+    ],
+  },
+  {
+    title: "Our Community",
+    links: ["Climbing", "Hiking"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Contact Us",
+  links: [
+    { label: "Admin Officer", value: "123-456-7890" },
+    { label: "Email Officer", value: "contact@explorez.com" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Social",
+  links: [
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/wordpress.svg",
+  ],
+};
